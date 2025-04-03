d66a1105d2b3bf96c07711f10287d83cb3b85a82
Waseem Azmy <waseemazmy21@gmail.com>
2025-04-03 18:15:00 +0200

    edit

--- pages/shop-slider-index.js
+++ pages/shop-slider-index.js
@@ -1,30 +1,39 @@
 let URL = "https://fakestoreapi.com/products";
 let XHR = new XMLHttpRequest();
-function getData(){
-    XHR.open("get", URL, true)
-    XHR.onload = function(){
-        if (XHR.status == 200){
-            let res = XHR.response;
-            let resObj = JSON.parse(res)
-            let productCard = document.getElementById("product-card");
-            
-            resObj.slice(1, 3).forEach(item => {
-                let productContainer = document.createElement("div");
-                productContainer.classList.add("card2");
-                productContainer.innerHTML = 
-               `<img src="${item.image}" alt="product Image">
-                    <div class="product-info2">
-                        <h2 class="product-title2">${item.title}</h2>
-                        <h3 class="product-des2">${item.category}</h3>
-                        <span class="original-price2">$${item.price}</span>
+
+// // single responsiblity
+// function getData() {}
+
+// const product = getData();
+
+// product.forEach((product) => creteProduct(product));
+
+// function creteProduct(product) {}
+
+function getData() {
+  XHR.open("get", URL, true);
+  XHR.onload = function () {
+    if (XHR.status == 200) {
+      let res = XHR.response;
+      let resObj = JSON.parse(res);
+      let productCard = document.querySelector(".products");
+
+      resObj.slice(1, 3).forEach((item) => {
+        let productContainer = document.createElement("div");
+        productContainer.classList.add("card");
+        productContainer.innerHTML = `<img src="${item.image}" alt="product Image">
+                    <div class="product-info">
+                        <h2 class="product-title">${item.title}</h2>
+                        <h3 class="product-des">${item.category}</h3>
+                        <span class="original-price">$${item.price}</span>
                         <p>lorem</p>
                     </div>   
                `;
-               productCard.appendChild(productContainer)
-            })
-            console.log(resObj)
-        }
+        productCard.appendChild(productContainer);
+      });
+      console.log(resObj);
     }
-    XHR.send()
+  };
+  XHR.send();
 }
-getData()
+getData();
