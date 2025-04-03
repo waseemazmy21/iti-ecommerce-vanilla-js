f45cd0a2c3abe6365dd1e9c288f4d24d7f8a9fc7
markosthabit <markosthabit@protonmail.com>
2025-04-03 16:42:25 +0200

    Added a method to automate the creation of product cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,33 @@
+import { createElement, createProductCard } from "./ui.js";
+import { getProductById, getProducts } from "./api.js"
+
+const app = document.getElementById("app");
+
+const page = createElement("div", { "class": "page" }, app);
+const header = createElement("div", { "class": "header" }, page);
+const header_left_section = createElement("div", { "class": "header-section" }, header);
+const mail = createElement("a", {"href": "mailto:emailaddress@whatever.com"}, header_left_section, "\u{2709} emailaddress@whatever.com")
+const tel = createElement("a", {"href": "tel:+564534545"}, header_left_section, "\u{260E} (123)4567890")
+const header_right_section = createElement("div", { "class": "header-section" }, header);
+const login = createElement("a", { "href": "link_to_login_page" }, header_right_section, "\u{1F464} Login");
+const cart = createElement("a", { "href": "link_to_cart_page" }, header_right_section, "\u{1F6D2}");
+const navbar = createElement("div", { "class": "navbar" }, page);
+const websiteName = createElement("h1", {}, navbar, "EShop!");
+const top_section = createElement("div", { "class": "top-section" }, page);
 
 
-function createProduct(product) {
-    const div = document.createElement('div');
-    div.className = 'product';
-    const productPrice = document.getElementById("product-price").value;
-    const productDescription = document.getElementById("product-description").value;
+// getProducts() => api to get products data from db
+var productsData = getProducts();
 
-    const product = {
-        name: productName,
-        price: productPrice,
-        description: productDescription
-    };
 
-    console.log(product);
-    alert("Product created successfully!");
+// building the productCards inside a fragment
+const fragment = new DocumentFragment();
+
+for (const product of productsData) {
+    const productCard = createProductCard(product);
+    fragment.appendChild(productCard);
 }
+
+
+
+page.appendChild(fragment)
---
+++ scripts/ui.js
@@ -0,0 +1,28 @@
+export function createElement(tag, attributes = {}, parent, textContent = "") {
+    const elem = document.createElement(tag);
+
+    // attributes = {"attribute name": "attribute value"}
+    Object.keys(attributes).forEach(attr => elem.setAttribute(attr, attributes[attr]));
+
+    if (textContent!= ""){
+        elem.textContent = textContent;
+    }
+
+    !!parent?parent.appendChild(elem):{};
+    return elem;
+}
+
+export function createProductCard(product) {
+    const prodCard = createElement("div", { "class": "card" });
+    const img = createElement("img", { "src": product["image"], "alt": product["title"] }, prodCard);
+    const prod_title = createElement("h5", {}, prodCard, product["title"]);
+    const circles = createElement("div", { "class": "circle-container" }, prodCard);
+    const butterup = createElement("div", { "class": "circle buttercup" }, circles);
+    const purple = createElement("div", { "class": "circle purple" }, circles);
+    const violet = createElement("div", { "class": "circle violet" }, circles);
+    const prices = createElement("div", { "class": "prices" }, prodCard);
+    const price = createElement("p", {"class":"price"}, prices, "$" + (product["price"] * .75).toFixed(2));
+    const original_price = createElement("p", {"class":"price original-price"}, prices,"$" + product["price"]);
+
+    return prodCard;
+}
