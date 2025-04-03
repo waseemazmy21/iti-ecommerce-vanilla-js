f87a503287507e088cfbdb6ca0226ef5a8755319
mohammed6699 <mohammedahmed.refaat@gmail.com>
2025-04-03 18:19:24 +0200

    update the data

--- pages/shop-slider-index.js
+++ pages/shop-slider-index.js
@@ -6,17 +6,17 @@ function getData(){
         if (XHR.status == 200){
             let res = XHR.response;
             let resObj = JSON.parse(res)
-            let productCard = document.getElementById("product-card");
+            let productCard = document.querySelector("product-card");
             
             resObj.slice(1, 3).forEach(item => {
                 let productContainer = document.createElement("div");
-                productContainer.classList.add("card2");
+                productContainer.classList.add("card");
                 productContainer.innerHTML = 
                `<img src="${item.image}" alt="product Image">
-                    <div class="product-info2">
-                        <h2 class="product-title2">${item.title}</h2>
-                        <h3 class="product-des2">${item.category}</h3>
-                        <span class="original-price2">$${item.price}</span>
+                    <div class="product-info">
+                        <h2 class="product-title">${item.title}</h2>
+                        <h3 class="product-des">${item.category}</h3>
+                        <span class="original-price">$${item.price}</span>
                         <p>lorem</p>
                     </div>   
                `;
