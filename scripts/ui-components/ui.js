47f0561be6beccb14f75b9e94349c0bd6b278b46
Waseem Azmy <waseemazmy21@gmail.com>
2025-04-11 18:11:27 +0200

    add addToCart function to landing page

--- scripts/ui-components/ui.js
+++ scripts/ui-components/ui.js
@@ -1,3 +1,5 @@
+
+
 export function createElement(tag, attributes = {}, parent, textContent = "") {
     const elem = document.createElement(tag);
 
@@ -12,10 +14,25 @@ export function createElement(tag, attributes = {}, parent, textContent = "") {
     return elem;
 }
 
+export function addToCart (product)  {
+    const cart = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+    const existingProduct = cart.find((item) => item.product.id === product.id);
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      cart.push({ product, quantity: 1 });
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+};
+
 export function createProductCard(product) {
     const prodCard = createElement("div", { "class": "card" });
     const img = createElement("img", { "src": product["image"], "alt": product["title"] }, prodCard);
-    const add_cart = createElement("a", {"class":"cart-button", "onclick":"alert('Item added to cart!')"}, prodCard, "\u{1F6D2}");
+    const add_cart = createElement("a", {"class":"cart-button"}, prodCard, "\u{1F6D2}");
+    add_cart.addEventListener("click", () => {
+        addToCart(product);
+        alert("Added to cart!");
+    });
     const prod_title = createElement("h5", {}, prodCard, product["title"].split(' ').slice(0, 5).join(' '));
     const circles = createElement("div", { "class": "circle-container" }, prodCard);
     const butterup = createElement("div", { "class": "circle buttercup" }, circles);
