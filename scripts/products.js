3b5949b9557a98ff02335c0a88a47bf75a8ab9c5
Waseem Azmy <waseemazmy21@gmail.com>
2025-04-11 17:35:17 +0200

    fix styling

--- scripts/products.js
+++ scripts/products.js
@@ -1,27 +1,26 @@
 let ApiURL = "https://fakestoreapi.com/products";
 let XHR = new XMLHttpRequest();
 
-function getData(Data){
-    XHR.open("get", ApiURL, true)
-    XHR.onload = function(){
-        if (XHR.status == 200){
-            let res = XHR.response;
-            let resObj = JSON.parse(res)
-            Data(resObj)
+function getData(Data) {
+  XHR.open("get", ApiURL, true);
+  XHR.onload = function () {
+    if (XHR.status == 200) {
+      let res = XHR.response;
+      let resObj = JSON.parse(res);
+      Data(resObj);
       // console.log(resObj);
     }
   };
   XHR.send();
 }
 getData();
-function printData(){
-    getData(function(resObj) {
-      let productCard = document.querySelector(".products");
-      resObj.forEach(item => {
-        let productContainer = document.createElement("div");
-        productContainer.classList.add("card");
-        productContainer.innerHTML = 
-        `<img src="${item.image}" alt="product Image">
+function printData() {
+  getData(function (resObj) {
+    let productCard = document.querySelector(".products");
+    resObj.forEach((item) => {
+      let productContainer = document.createElement("div");
+      productContainer.classList.add("card");
+      productContainer.innerHTML = `<img src="${item.image}" alt="product Image">
         <div class="product-info">
         <h2 class="product-title">${item.title}</h2>
         <h3 class="product-des">${item.category}</h3>
@@ -31,69 +30,75 @@ function printData(){
         <a href="product.html?id=${item.id}"><button class="card-submit-btn">Show more</button></a>
         </div>   
         `;
-        productCard.appendChild(productContainer);
-        });
-      });
+      productCard.appendChild(productContainer);
+    });
+  });
 }
-printData()
-/* search Data function*/ 
-function searchData(){
- let searchBar = document.getElementById("search-Data").value.trim().toLowerCase();
- XHR = new XMLHttpRequest();
- XHR.open("get", ApiURL, true)
- XHR.onreadystatechange = function(){
-  if (XHR.readyState == 4 && XHR.status == 200){
-    let result = XHR.response;
-    let objectResult = JSON.parse(result)
-    let filterData = objectResult.filter(product => 
-      product.title.toLowerCase().includes(searchBar)
-    );
-    let searchElement = document.querySelector(".products");
-    searchElement.innerHTML = "";
-    for (product of filterData){
-      let productContainer = document.createElement("div");
-      productContainer.classList.add("card");
-      productContainer.innerHTML = `
+printData();
+/* search Data function*/
+function searchData() {
+  let searchBar = document.getElementById("search").value.trim().toLowerCase();
+  XHR = new XMLHttpRequest();
+  XHR.open("get", ApiURL, true);
+  XHR.onreadystatechange = function () {
+    if (XHR.readyState == 4 && XHR.status == 200) {
+      let result = XHR.response;
+      let objectResult = JSON.parse(result);
+      let filterData = objectResult.filter((product) =>
+        product.title.toLowerCase().includes(searchBar)
+      );
+      let searchElement = document.querySelector(".products");
+      searchElement.innerHTML = "";
+      for (product of filterData) {
+        let productContainer = document.createElement("div");
+        productContainer.classList.add("card");
+        productContainer.innerHTML = `
       <img src="${product.image}" alt="product Image">
         <div class="product-info">
         <h2 class="product-title">${product.title}</h2>
         <h3 class="product-des">${product.category}</h3>
         <span class="original-price">$${product.price}</span>
-        <p>${product.description}</p>
         <p>Rate: ⭐${product.rating.rate} </p>
         <p>Number of reviews: (${product.rating.count}) review</p>
         <a href="product.html?id=${product.id}"><button class="card-submit-btn">Learn More</button></a>
         </div>
       `;
-      searchElement.appendChild(productContainer);
+        searchElement.appendChild(productContainer);
+      }
+      console.log(filterData);
     }
-    console.log(filterData) 
-  }
- }
-  XHR.send()
+  };
+  XHR.send();
 }
 
+const searchButton = document.getElementById("search-button");
+searchButton.addEventListener("click", searchData);
+
 /*search data by categories and price amd number slice */
-function searchCategory(){
-  let selectedValue = document.getElementById("select-product").value.toLowerCase();
+function searchCategory() {
+  let selectedValue = document
+    .getElementById("select-product")
+    .value.toLowerCase();
   let selectedNumber = document.getElementById("page-number").value || 20;
-  
+
   let productContainer = document.createElement("div");
   productContainer.classList.add("card");
   let XHR = new XMLHttpRequest();
 
   XHR.open("get", ApiURL, true);
-  XHR.onreadystatechange = function(){
-    if (XHR.readyState == 4 && XHR.status == 200){
+  XHR.onreadystatechange = function () {
+    if (XHR.readyState == 4 && XHR.status == 200) {
       let result = XHR.response;
       let ObjectResult = JSON.parse(result);
       let sortData;
-/********************************************************* */
-      if (selectedValue == "category"){
-        sortData = ObjectResult.sort((a, b) => a.category.localeCompare(b.category))
+      /********************************************************* */
+      if (selectedValue == "category") {
+        sortData = ObjectResult.sort((a, b) =>
+          a.category.localeCompare(b.category)
+        );
         let searchElement = document.querySelector(".products");
         searchElement.innerHTML = "";
-        for(productName of sortData){
+        for (productName of sortData) {
           let productContainer = document.createElement("div");
           productContainer.classList.add("card");
           productContainer.innerHTML = `
@@ -102,22 +107,23 @@ function searchCategory(){
         <h2 class="product-title">${productName.title}</h2>
         <h3 class="product-des">${productName.category}</h3>
         <span class="original-price">$${productName.price}</span>
-        <p>${productName.description}</p>
         <p>Rate: ⭐${productName.rating.rate}</p>
         <p>Number of reviews: (${productName.rating.count}) review</p>
         <a href="product.html?id=${productName.id}"><button class="card-submit-btn">Learn More</button></a>
         </div>`;
           searchElement.appendChild(productContainer);
         }
-        console.log(sortData)
+        console.log(sortData);
 
         /********************************************* */
-      } else if (selectedValue == "price"){
-        sortData = ObjectResult.sort((num1, num2) => Number(num1.price) - Number(num2.price))
+      } else if (selectedValue == "price") {
+        sortData = ObjectResult.sort(
+          (num1, num2) => Number(num1.price) - Number(num2.price)
+        );
         let elementPrice = document.querySelector(".products");
         elementPrice.innerHTML = "";
-        for (product of sortData){
-          let productsContainer = document.createElement("div")
+        for (product of sortData) {
+          let productsContainer = document.createElement("div");
           productsContainer.classList.add("card");
           productsContainer.innerHTML = `
           <img src="${product.image}" alt="product Image">
@@ -125,7 +131,6 @@ function searchCategory(){
           <h2 class="product-title">${product.title}</h2>
           <h3 class="product-des">${product.category}</h3>
           <span class="original-price">$${product.price}</span>
-          <p>${product.description}</p>
           <p>Rate: ⭐${product.rating.rate}</p>
           <p>Available products: ${product.rating.count}</p>
           <a href="product.html?id=${product.id}">
@@ -134,63 +139,60 @@ function searchCategory(){
           </div>`;
           elementPrice.appendChild(productsContainer);
         }
-        console.log(sortData)
+        console.log(sortData);
       } else {
         sortData = ObjectResult;
-        console.log(sortData)
+        console.log(sortData);
       }
-      sortData = sortData.slice(0, selectedNumber)
+      sortData = sortData.slice(0, selectedNumber);
       let searchElement = document.querySelector(".products");
       searchElement.innerHTML = "";
-      for (productPage of sortData){
+      for (productPage of sortData) {
         let productPageContainer = document.createElement("div");
-        productPageContainer.classList.add("card")
+        productPageContainer.classList.add("card");
         productPageContainer.innerHTML += `
         <img src="${productPage.image}" alt="product Image">
         <div class="product-info">
         <h2 class="product-title">${productPage.title}</h2>
         <h3 class="product-des">${productPage.category}</h3>
         <span class="original-price">$${productPage.price}</span>
-        <p>${productPage.description}</p>
         <p>Rate: ⭐${productPage.rating.rate}</p>
         <p>Available products: (${productPage.rating.count}) review</p>
         <a href="product.html?id=${productPage.id}">
           <button class="card-submit-btn">Learn More</button>
           </a>
-        </div>`
-        ;
+        </div>`;
         searchElement.appendChild(productPageContainer);
       }
-      console.log(sortData)
+      console.log(sortData);
     }
-  }
-  XHR.send()
+  };
+  XHR.send();
 }
 /*The max and min price checkbox */
-function filterPrice(){
+function filterPrice() {
   let searchElement = document.querySelector(".products");
-  let EnterPrice = Number(document.getElementById("Price").value)
+  let EnterPrice = Number(document.getElementById("Price").value);
   let XHR = new XMLHttpRequest();
   XHR.open("get", ApiURL, true);
-  XHR.onreadystatechange = function(){
-    if(XHR.readyState == 4 && XHR.status == 200){
+  XHR.onreadystatechange = function () {
+    if (XHR.readyState == 4 && XHR.status == 200) {
       let PriceResult = XHR.response;
       let PriceResultObj = JSON.parse(PriceResult);
       searchElement.innerHTML = "";
 
-      for (products of PriceResultObj){
-        let productPrice = Number(products.price)
-        switch(true){
-          case(productPrice >= EnterPrice - 2 && productPrice <= EnterPrice + 2):
-          let productContainer = document.createElement("div");
-          productContainer.classList.add("card");
-          productContainer.innerHTML = `
+      for (products of PriceResultObj) {
+        let productPrice = Number(products.price);
+        switch (true) {
+          case productPrice >= EnterPrice - 2 && productPrice <= EnterPrice + 2:
+            let productContainer = document.createElement("div");
+            productContainer.classList.add("card");
+            productContainer.innerHTML = `
             <img src="${products.image}" alt="product Image">
         <div class="product-info">
         <h2 class="product-title">${products.title}</h2>
         <h3 class="product-des">${products.category}</h3>
         <span class="original-price">$${products.price}</span>
-        <p>${products.description}</p>
         <p>Rate: ⭐${products.rating.rate}</p>
         <p>Available products: (${products.rating.count}) review</p>
         <a href="product.html?id=${products.id}">
@@ -198,15 +200,14 @@ function filterPrice(){
           </a>
         </div>
           `;
-          searchElement.appendChild(productContainer);
-          break;
+            searchElement.appendChild(productContainer);
+            break;
         }
       }
-      if(searchElement.innerHTML == ""){
-        searchElement.innerHTML = `<p>No products Found Under this Price</p>`
+      if (searchElement.innerHTML == "") {
+        searchElement.innerHTML = `<p>No products Found Under this Price</p>`;
       }
-      
     }
-  }
-XHR.send()
+  };
+  XHR.send();
 }
